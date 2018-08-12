4503a333384ed656342bfac706df015c2eecf5b4
Miles Johnson <mileswjohnson@gmail.com>
2018-08-12 00:39:05 -0700

    Add complex tests.

---
+++ tests/fixtures/type-complex.ts
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export interface Props {
+  a: number;
+  b?: string | boolean;
+  c: 1 | 'foo' | false;
+  d?: { name?: string }[];
+  e: {
+    e1: string[][];
+    e2: () => void;
+    e3?: { e1a: object } | null;
+  };
+  f?: any;
+  g: ((e: Event) => null) | (() => {});
+  h?: { [key: string]: number } & { [key: string]: string[] };
+}
+
+export default class TypeComplex extends React.Component<Props> {
+  render() {
+    return null;
+  }
+}
