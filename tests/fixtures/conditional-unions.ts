8d09d4c8fc36554bbf82b6bc1888eb607c0134b5
Miles Johnson <mileswjohnson@gmail.com>
2020-01-25 18:34:42 -0800

    test: Add conditional union test.

---
+++ tests/fixtures/conditional-unions.ts
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface BasicProps {
+  foo: boolean;
+}
+
+interface ConditionA extends BasicProps {
+  A: string;
+  B?: string;
+}
+
+interface ConditionB extends BasicProps {
+  A?: string;
+  B: string;
+}
+
+export interface Props {
+  name: ConditionA | ConditionB;
+}
+
+export default class ConditionalUnions extends React.Component<Props> {
+  render() {
+    return null;
+  }
+}
